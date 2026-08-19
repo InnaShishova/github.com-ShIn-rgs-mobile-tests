b547993929221781019d23e5bd832a09ed6854a9
Inna Shishova <sh201608@yandex.ru>
2026-08-19 19:12:13 +0400

    Fix BrowserStack Appium capabilities

---
+++ README.md
@@ -0,0 +1,31 @@
+# Mobile UI Automation Project
+
+Проект мобильной автоматизации тестирования приложения Wikipedia для Android.
+
+## Technology Stack
+
+- Java 17
+- JUnit 5
+- Appium
+- BrowserStack
+- Owner
+- Allure Report
+- Gradle
+- Git
+- GitHub
+
+## Implemented Tests
+
+- Поиск статьи в Wikipedia
+- Открытие статьи из результатов поиска
+- Проверка открытия экрана поиска
+
+## Project Structure
+
+```text
+src/test/java
+├── config
+├── drivers
+├── screens
+├── tests
+└── utils
--- src/test/java/drivers/BrowserstackDriver.java
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -2,12 +2,14 @@ package drivers;
 
 import config.MobileConfig;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.MutableCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Map;
 
 public class BrowserstackDriver {
 
@@ -30,36 +32,51 @@ public class BrowserstackDriver {
             );
         }
 
-        MutableCapabilities capabilities = new MutableCapabilities();
+        UiAutomator2Options options = new UiAutomator2Options();
 
-        HashMap<String, Object> bstackOptions = new HashMap<>();
+        // =========================
+        // Appium capabilities
+        // =========================
+        options.setPlatformName("Android");
+        options.setDeviceName(config.device());
+        options.setPlatformVersion(config.osVersion());
+        options.setAutomationName("UiAutomator2");
+        options.setApp(config.app());
+
+        // =========================
+        // BrowserStack capabilities
+        // =========================
+        Map<String, Object> bstackOptions = new HashMap<>();
+
         bstackOptions.put("userName", username);
         bstackOptions.put("accessKey", accessKey);
-        bstackOptions.put("deviceName", config.device());
-        bstackOptions.put("osVersion", config.osVersion());
-        bstackOptions.put("projectName", "Mobile Diploma");
-        bstackOptions.put("buildName", "rgs-mobile-tests");
-        bstackOptions.put("sessionName", "Wikipedia Android Test");
 
-        capabilities.setCapability(
+        bstackOptions.put(
+                "projectName",
+                "Mobile Diploma"
+        );
+
+        bstackOptions.put(
+                "buildName",
+                "rgs-mobile-tests"
+        );
+
+        bstackOptions.put(
+                "sessionName",
+                "Wikipedia Android Test"
+        );
+
+        options.setCapability(
                 "bstack:options",
                 bstackOptions
         );
 
-        capabilities.setCapability(
-                "appium:app",
-                config.app()
-        );
-
-        capabilities.setCapability(
-                "appium:automationName",
-                "UiAutomator2"
-        );
-
         try {
             return new AndroidDriver(
-                    new URL("https://hub.browserstack.com/wd/hub"),
-                    capabilities
+                    new URL(
+                            "https://hub.browserstack.com/wd/hub"
+                    ),
+                    options
             );
         } catch (MalformedURLException e) {
             throw new RuntimeException(
